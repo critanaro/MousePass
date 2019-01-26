b63bf3b8c11c136e366e19e548e3faee135a7b82
tGoh98 <tmg5@rice.edu>
2019-01-26 17:10:12 -0600

    Update MousePass.java
    added yes button

--- MousePass.java
+++ MousePass.java
@@ -9,9 +9,12 @@ public class MousePass {
     public static void main(String[] args) {
         JFrame f=new JFrame();//creating instance of JFrame  
           
+        JButton no = new JButton("No");//creating instance of JButton  
+        no.setBounds(50,50,200, 80);//x axis, y axis, width, height  
+        f.add(no);//adding button in JFrame  
+        
         JButton yes = new JButton("Yes");//creating instance of JButton  
-        yes.setBounds(50,50,200, 80);//x axis, y axis, width, height  
-
+        yes.setBounds(1225,50,200, 80);//x axis, y axis, width, height  
         f.add(yes);//adding button in JFrame  
 
         f.setSize(1500,750);//400 width and 500 height  
