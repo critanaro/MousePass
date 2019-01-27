a789fdd11af5cfb65ce9edbe23c44081ab7b992a
firemike0 <michael.sprintson@gmail.com>
2019-01-26 18:54:28 -0600

    edited mousepass and began to implement

--- mouspass.py
+++ mouspass.py
@@ -8,31 +8,32 @@ import matplotlib.pyplot as plt
 
 print(tf.__version__)
 
-fashion_mnist = keras.datasets.fashion_mnist
+#fashion_mnist = keras.datasets.fashion_mnist
+#(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+#Import the DataSet
+
+train_images = [[(0,0),(0,0),(1,2),(3,4),(6,7),(9,11),(0,0)],[(0,0),(6,5),(3,4),(7,6),(10,4),(9,13),(0,0)]]
+train_images = np.asarray(train_images)
+train_labels = [1,0]
+train_labels = np.asarray(train_labels)
+class_names = ['True','False']
+
+#class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 #test
-train_images = train_images / 255.0
+#train_images = train_images / 255.0
 
-test_images = test_images / 255.0
+#test_images = test_images / 255.0
 
-plt.figure(figsize=(10, 10))
-for i in range(25):
-    plt.subplot(5, 5, i + 1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-# plt.show()
+
+#Build the model
+
 
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
+    #keras.layers.Flatten(input_shape=(28, 28)), - not needed as this takes a 2d array for each pic and transforms it into a 1d array.
     keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
+    keras.layers.Dense(2, activation=tf.nn.softmax)
 ])
 
 model.compile(optimizer='adam',
@@ -45,9 +46,6 @@ test_loss, test_acc = model.evaluate(test_images, test_labels)
 
 print('Test accuracy:', test_acc)
 
-num_rows = 5
-num_cols = 3
-num_images = num_rows * num_cols
 
 img = test_images[0]
 img = (np.expand_dims(img, 0))
