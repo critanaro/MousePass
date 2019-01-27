7dd22fb4dc425519e9aee5aaf3f6df5dc727a1c2
Nickolas Chen <critanaro@gmail.com>
2019-01-26 21:40:10 -0600

    Update mouspass.py
    Changed mouspass.py to save checkpoints

--- mouspass.py
+++ mouspass.py
@@ -1,3 +1,4 @@
+import os
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
@@ -33,21 +34,72 @@ class_names = ['True','False']
 #test_images = test_images / 255.0
 
 
+
+
 #Build the model
 
 
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(7, 2)),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(2, activation=tf.nn.softmax)
-])
+def create_model():
+	model = keras.Sequential([
+    	keras.layers.Flatten(input_shape=(7, 2)),
+    	keras.layers.Dense(128, activation=tf.nn.relu),
+    	keras.layers.Dense(2, activation=tf.nn.softmax)
+	])
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+	model.compile(optimizer='adam',
+        	      loss=tf.keras.losses.sparse_categorical_crossentropy,
+        	      metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=5)
+	return model 
 
+#create checkpoints
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+
+# Create checkpoint callback
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1, period=5)#saves every 5 epochs
+
+#stores training data and gets it back
+model = create_model()
+
+model.fit(train_images, train_labels,  epochs = 50,
+			validation_data = (test_images,test_labels),
+          	callbacks = [cp_callback], verbose =0)  # pass callback to training
+
+
+
+loss, acc = model.evaluate(test_images, test_labels)
+print("Untrained model, accuracy: {:5.2f}%".format(100*acc))
+
+
+model.load_weights(checkpoint_path)
+loss,acc = model.evaluate(test_images, test_labels)
+print("Restored model, accuracy: {:5.2f}%".format(100*acc))
+
+
+#test that shit
+# include the epoch in the file name. (uses `str.format`)
+checkpoint_path = "training_2/cp-{epoch:04d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    checkpoint_path, verbose=1, save_weights_only=True,
+    # Save weights, every 5-epochs.
+    period=5)
+
+model = create_model()
+model.save_weights(checkpoint_path.format(epoch=0))
+model.fit(train_images, train_labels,
+          epochs = 50, callbacks = [cp_callback],
+          validation_data = (test_images,test_labels),
+          verbose=0)
+
+latest = tf.train.latest_checkpoint(checkpoint_dir)
+#tests training data
+"""
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 
 print('Test accuracy:', test_acc)
@@ -60,3 +112,4 @@ predictions_single = model.predict(img)
 print(predictions_single)
 x = np.argmax(predictions_single[0])
 print(x)
+"""
