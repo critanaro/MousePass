15c675fcb2fe6ed9a7b6be41e08dacd0e97eed75
Nickolas Chen <critanaro@gmail.com>
2019-01-26 23:22:28 -0600

    yeep
    yert

--- mouspass.py
+++ mouspass.py
@@ -40,7 +40,7 @@ class_names = ['True','False']
 
 
 def create_model():
-	model = keras.Sequential([
+	model = tf.keras.models.Sequential([
     	keras.layers.Flatten(input_shape=(7, 2)),
     	keras.layers.Dense(128, activation=tf.nn.relu),
     	keras.layers.Dense(2, activation=tf.nn.softmax)
@@ -52,23 +52,24 @@ def create_model():
 
 	return model 
 
+model = create_model()
+model.summary()
+
 #create checkpoints
 checkpoint_path = "training_1/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
-
 # Create checkpoint callback
 cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
                                                  save_weights_only=True,
-                                                 verbose=1, period=5)#saves every 5 epochs
+                                                 verbose=1)
 
 #stores training data and gets it back
 model = create_model()
 
-model.fit(train_images, train_labels,  epochs = 50,
-			validation_data = (test_images,test_labels),
-          	callbacks = [cp_callback], verbose =0)  # pass callback to training
-
+model.fit(train_images, train_labels,  epochs = 10,
+           validation_data = (test_images,test_labels),
+           callbacks = [cp_callback], verbose =0)  # pass callback to training
 
 
 loss, acc = model.evaluate(test_images, test_labels)
@@ -78,28 +79,8 @@ print("Untrained model, accuracy: {:5.2f}%".format(100*acc))
 model.load_weights(checkpoint_path)
 loss,acc = model.evaluate(test_images, test_labels)
 print("Restored model, accuracy: {:5.2f}%".format(100*acc))
-
-
-#test that shit
-# include the epoch in the file name. (uses `str.format`)
-checkpoint_path = "training_2/cp-{epoch:04d}.ckpt"
-checkpoint_dir = os.path.dirname(checkpoint_path)
-
-cp_callback = tf.keras.callbacks.ModelCheckpoint(
-    checkpoint_path, verbose=1, save_weights_only=True,
-    # Save weights, every 5-epochs.
-    period=5)
-
-model = create_model()
-model.save_weights(checkpoint_path.format(epoch=0))
-model.fit(train_images, train_labels,
-          epochs = 50, callbacks = [cp_callback],
-          validation_data = (test_images,test_labels),
-          verbose=0)
-
-latest = tf.train.latest_checkpoint(checkpoint_dir)
-#tests training data
 """
+#testing
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 
 print('Test accuracy:', test_acc)
